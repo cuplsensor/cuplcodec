debf57b147007e573cdc7264f47fcbf914f16cf2
Malcolm Mackay <malcolm.mackay121@gmail.com>
2019-03-10 11:12:10 +0000

    More documentation added.

--- docs/reference/encoder.rst
+++ docs/reference/encoder.rst
@@ -4,3 +4,5 @@ URL Encoder Reference
 :ref:`sample-c-reference`
 
 :ref:`samplehist-c-reference`
+
+:ref:`nvtype-c-reference`
--- encoder/pyencoder/c_encoder/nvtype.h
+++ encoder/pyencoder/c_encoder/nvtype.h
@@ -9,22 +9,24 @@
 #define COMMS_NVTYPE_H_
 
 
-#define SERIAL_LENBYTES     8
+#define SERIAL_LENBYTES     8  /*!< Length of unique box serial. */
 #define SECKEY_LENBYTES     8
 #define SMPLINT_LENBYTES     2
 #define RANDSTATE_LENBYTES      4
 #define INTEGERFIELD_LENBYTES   4
 
-
+/**
+ *  Structure to hold configuration data held in non-volatile memory.
+ */
 typedef struct nvstruct {
-    char serial[SERIAL_LENBYTES];
-    char seckey[SECKEY_LENBYTES];
-    char smplintervalmins[SMPLINT_LENBYTES];
-    unsigned int randstate;
-    unsigned int sleepintervaldays;
-    unsigned int allwritten;
-    unsigned int resetsperloop;
-    unsigned int resetsalltime;
+    char serial[SERIAL_LENBYTES];   /*!< Unique box serial array. */
+    char seckey[SECKEY_LENBYTES];   /*!< Secret key array. This is used to generate the HMAC. */
+    char smplintervalmins[SMPLINT_LENBYTES]; /*!< Time interval betweeen samples in minutes. This is b64 encoded so it is easier to store it as an array. */
+    unsigned int randstate; /*!< Version. */
+    unsigned int sleepintervaldays; /*!< The number of days to wait without scans before putting the sensor into deep sleep mode. */
+    unsigned int allwritten;        /*!< Indicates that all required NV parameters have been set. */
+    unsigned int resetsperloop;     /*!< Incremented each time the microcontroller resets. Zeroed when the circular buffer has wraps from the end back to the beginning. */
+    unsigned int resetsalltime;     /*!< Incremented each time the microcontroller resets. Never set to zero. */
 } nv_t;
 
 #endif /* COMMS_NVTYPE_H_ */
