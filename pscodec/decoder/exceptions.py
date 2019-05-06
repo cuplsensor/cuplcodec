3acfecc8a62ffc672ca1c26493d10c873f6e2756
Malcolm Mackay <malcolm.mackay121@gmail.com>
2019-05-06 16:48:21 +0100

    Refactored exceptions.

--- pscodec/decoder/exceptions.py
+++ pscodec/decoder/exceptions.py
@@ -43,3 +43,33 @@ class NoCircularBufferError(ParamDecoderError):
     def __init__(self, status):
         super().__init__()
         self.status = status
+
+
+class CircularBufferError(PSCodecError):
+    """Base application error class."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class MessageIntegrityError(CircularBufferError):
+    errormsg = "MD5 checksum mismatch. Calculated MD5 = {}, URL MD5 = {}"
+
+    def __init__(self, calcmd5, urlmd5):
+        super().__init__()
+        self.calcmd5 = calcmd5
+        self.urlmd5 = urlmd5
+
+    def __str__(self):
+        return self.errormsg.format(self.calcmd5, self.urlmd5)
+
+
+class DelimiterNotFoundError(CircularBufferError):
+    errormsg = " No delimiting character found in the circular buffer string = {}. "
+
+    def __init__(self, encstr):
+        super().__init__()
+        self.encstr = encstr
+
+    def __str__(self):
+        return self.errormsg.format(self.encstr)
