37ca2f6ce5c3398d9309396e03169b5baa20af51
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-04-13 08:59:16 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -53,6 +53,7 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
     'breathe',
+    'sphinx_rtd_theme',
 ]
 
 breathe_projects = { "encoderproject": "wscodec/encoder/pyencoder/c_encoder/doxygen/xml/"} # BREATHE
@@ -91,7 +92,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'navigation_depth': 4,
+}
 html_logo = 'pslogo.png'
 html_favicon = 'pslogo.ico'
 
