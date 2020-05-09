eda2c8809682a1022d6dfa23ccf1de49712e8162
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-09 16:30:01 +0100

    Update test_tsample.py

--- tests/test_tsample.py
+++ tests/test_tsample.py
@@ -1,6 +1,7 @@
 import pytest
 from wscodec.encoder.pyencoder.instrumented import InstrumentedSampleT
 
+INPUT_BASEURL = "cupl.uk"
 INPUT_SERIAL = 'abcdabcd'
 INPUT_TIMEINT = 12
 INPUT_SECKEY = 'AAAABBBBCCCCDDDD'
@@ -31,6 +32,31 @@ def comparelists(urllist, inlist):
 def test_lists_equal(instr_sample_populated):
     inlist = instr_sample_populated['inlist']
     urllist = instr_sample_populated['urllist']
+
+    comparelists(urllist, inlist)
+
+
+@pytest.mark.parametrize('n', range(1, 500, 1))
+def test_md5(instr_sample, n):
+    instr_md5 = InstrumentedSampleT(baseurl=INPUT_BASEURL,
+                                    serial=INPUT_SERIAL,
+                                    secretkey="",
+                                    smplintervalmins=INPUT_TIMEINT,
+                                    usehmac=False
+                                    )
+
+    inlist = instr_md5.pushsamples(n)
+
+    # Decode the URL
+    par = instr_md5.eepromba.get_url_parsedqs()
+    decodedurl = Decoder(secretkey="", statb64=par['x'][0], timeintb64=par['t'][0],
+                         circb64=par['q'][0], ver=par['v'][0], usehmac=False)
+
+    urllist = decodedurl.params.buffer.smpls
+    for d in urllist:
+        del d['ts']
+
+    inlist = inlist[:len(urllist)]
 
     comparelists(urllist, inlist)
 
