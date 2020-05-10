13d02a6bedf1ebdf3f7e9586d2c980d856900200
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-10 12:35:54 +0100

    Update pairhist.h

--- wscodec/encoder/pyencoder/c_encoder/pairhist.h
+++ wscodec/encoder/pyencoder/c_encoder/pairhist.h
@@ -4,11 +4,19 @@
  * in the NDEF message circular buffer that is stored on the NFC-readable EEPROM.
  *
  * Its purpose is to allow a hash to be taken of the unencoded circular buffer contents. The decoder uses this hash
- * to verify that it has decoded the circular buffer properly; outputting the same samples in the same order.
+ * to verify that it has decoded the circular buffer properly; outputting the same pairs in the same order. After
+ * decoding the pairs, the hash is calculated. The decoder checks this equals the encoder hash, which is extracted from
+ * #hashnb64 in the NDEF message.
  *
  * The <a href="https://en.wikipedia.org/wiki/MD5">MD5</a> algorithm is used by default.
- * It is simple enough to be used on a microcontroller with limited resources. As an option,
- * the <a href="https://en.wikipedia.org/wiki/HMAC">HMAC-MD5</a> hash can be used instead.
+ * It is simple enough to be used on a microcontroller with limited resources.
+ *
+ * As an option, the <a href="https://en.wikipedia.org/wiki/HMAC">HMAC-MD5</a> hash can be used instead. This is used for
+ * verifying authenticity in addition to message integrity. The encoder and decoder both possess a shared
+ * #secretkey. HMAC applies the MD5 in two rounds. On the second round, the first MD5 is concatenated with the
+ * secret key. The output is an MD5 hash. The decoder can only obtain an identical result if it has the same circular
+ * buffer data and secret key. The irreversibility of the hashing algorithm ensures that the secret key cannot be
+ * obtained by a malicious actor.
  *
  * When a pair is added or changed in the NDEF message, then the same change must be made to pairhistory.
  */
