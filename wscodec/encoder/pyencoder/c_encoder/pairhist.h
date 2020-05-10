7b7b4983e833aa71fdbd285634e832060a7065c4
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-10 13:09:18 +0100

    Update pairhist.h

--- wscodec/encoder/pyencoder/c_encoder/pairhist.h
+++ wscodec/encoder/pyencoder/c_encoder/pairhist.h
@@ -1,24 +1,27 @@
 /*!
  * @file pairhist.h
- * @brief This maintains a circular buffer named #pairhistory, which is stored in RAM. It contains all pairs that are
- * in the NDEF message circular buffer that is stored on the NFC-readable EEPROM.
+ * @brief This maintains a circular buffer named #pairhistory. It contains all pairs that are
+ * in the NDEF message circular buffer stored in the NFC-readable EEPROM. A crucial difference is that #pairhistory
+ * is stored in RAM, so its contents can be accessed quickly.
  *
- * Its purpose is to allow a hash to be taken of the unencoded circular buffer contents. The decoder uses this hash
- * to verify that it has decoded the circular buffer properly; outputting the same pairs in the same order. After
- * decoding the pairs, the hash is calculated. The decoder checks this equals the encoder hash, which is extracted from
- * #hashnb64 in the NDEF message.
+ * This allows for a hash to be taken of the unencoded circular buffer pairs, each time this list changes. The decoder uses this
+ * to verify that it has decoded the circular buffer faithfully: It must output the same list of pairs as supplied to the encoder
+ * in multiple calls to cbuf_pushsample().
+ *
+ * After decoding the circular buffer, the hash is calculated. The decoder checks this equals the encoder hash, which is extracted from
+ * #endstop_t::hashnb64 in the NDEF message. If it does not, an error is raised and no data are returned.
  *
  * The <a href="https://en.wikipedia.org/wiki/MD5">MD5</a> algorithm is used by default.
  * It is simple enough to be used on a microcontroller with limited resources.
  *
  * As an option, the <a href="https://en.wikipedia.org/wiki/HMAC">HMAC-MD5</a> hash can be used instead. This is used for
  * verifying authenticity in addition to message integrity. The encoder and decoder both possess a shared
- * #secretkey. HMAC applies the MD5 in two rounds. On the second round, the first MD5 is concatenated with the
+ * #nvstruct::seckey. HMAC applies the MD5 in two rounds. On the second round, the first MD5 is concatenated with the
  * secret key. The output is an MD5 hash. The decoder can only obtain an identical result if it has the same circular
- * buffer data and secret key. The irreversibility of the hashing algorithm ensures that the secret key cannot be
- * obtained by a malicious actor.
+ * buffer data and secret key. The irreversibility of the hashing algorithm protects the secret key.
  *
- * When a pair is added or changed in the NDEF message, then the same change must be made to pairhistory.
+ * When a pair is pushed to or overwritten in the NDEF message, then pairhistory must be updated with pairhist_push() and
+ * pairhist_ovr() respectively. This ensures that the output of pairhist_hash() will be accurate.
  */
 
 #ifndef _PAIRHIST_H_
@@ -48,8 +51,8 @@ typedef struct
 
 /// Have doxygen ignore this
 /// @cond
-int pairhist_ovr(pair_t sample);
-int pairhist_push(pair_t sample);
+int pairhist_ovr(pair_t pair);
+int pairhist_push(pair_t pair);
 hashn_t pairhist_hash(int npairs, int usehmac, unsigned int loopcount, unsigned int resetsalltime, unsigned int batv_resetcause, int cursorpos);
 pair_t pairhist_read(unsigned int index, int * error);
 
