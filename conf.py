8759945109db96ebd85f3295e10166a1f51ff095
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-10 17:24:37 +0100

    test

--- conf.py
+++ conf.py
@@ -129,7 +129,11 @@ html_logo = 'cupl_textonly_white_small.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_doc_static']
+html_static_path = ['docs/static']
+
+html_css_files = [
+    'custom.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
