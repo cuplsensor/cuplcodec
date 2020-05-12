34abefab8faec8de97eb641ed56a6d69689d474e
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-12 15:54:32 +0100

    t

--- wscodec/encoder/pyencoder/c_encoder/demi.h
+++ wscodec/encoder/pyencoder/c_encoder/demi.h
@@ -1,9 +1,9 @@
 #ifndef _DEMI_H_
 #define _DEMI_H_
 
-#define DEMI0 0
-#define DEMI1 1
-#define DEMI2 2
+//#define DEMI0 0
+//#define DEMI1 1
+//#define DEMI2 2
 
 typedef enum DemiState {
   ds_consecutive,
