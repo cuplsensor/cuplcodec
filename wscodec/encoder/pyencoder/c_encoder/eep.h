ea4882e82ada8b35e3c07e30f1edb36a76abb6fd
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-13 11:45:08 +0100

    made changes to eep.

--- wscodec/encoder/pyencoder/c_encoder/eep.h
+++ wscodec/encoder/pyencoder/c_encoder/eep.h
@@ -4,6 +4,7 @@
 int eep_write(const int eepblk, const unsigned int bufblk);
 int eep_read(const int eepblk, const unsigned int bufblk);
 int eep_cp(int * indexptr, const char * dataptr, const int lenbytes);
+int eep_swap(const unsigned int srcblk, const unsigned int destblk);
 int eep_cpbyte(int * indexptr, const char bytedata);
 void eep_waitwritedone(void);
 
