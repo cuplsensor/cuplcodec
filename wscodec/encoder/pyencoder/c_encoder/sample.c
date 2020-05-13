1ea12cea55130b08046adca298052fc855c1431a
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-13 11:17:44 +0100

    Update sample.c

--- wscodec/encoder/pyencoder/c_encoder/sample.c
+++ wscodec/encoder/pyencoder/c_encoder/sample.c
@@ -15,7 +15,7 @@
 #define BATV_RESETCAUSE(BATV, RSTC) ((BATV << 8) | (RSTC & 0xFF)) /*!< Macro for creating a 16-bit batv_resetcause value from 8-bit CODEC_FEAT_30 and CODEC_SPEC_16 values. */
 
 typedef enum {
-    initial,
+    pairbuf_init,
     pair0_both,         /*!< Write pair0 */
     pair0_reading1,     /*!< Overwrite reading1 of pair0 */
     pair1_both,         /*!< Write pair1 */
@@ -139,7 +139,7 @@ void sample_init(unsigned int resetcause, bool err)
   Base64encode(statusb64, (const char *)&status, sizeof(status));
 
   npairs = 0;
-  state = initial;
+  state = pairbuf_init;
 
   if (err == true)
   {
@@ -190,18 +190,8 @@ int cbuf_pushsample(int rd0, int rd1)
   switch(state)
       {
       case pair0_both:
-          switch(demi_movecursor())
-          {
-          case ds_looparound:
-            overwriting = 1;
-            break;
-          case ds_newloop:
-            incr_loopcounter();
-            break;
-          default:
-            break;
-          }
-      case initial:
+          break;
+      case pairbuf_init:
           set_pair(&pairbuf[0], rd0, rd1);
           set_pair(&pairbuf[1], 0, 0);
           npairs = overwriting ? (npairs + 1 - PAIRS_PER_DEMI) : (npairs + 1);
