4780ba56ffb2b3e0be2c8b405b6fdaf6fe413105
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-06-15 19:01:13 +0100

    Update decoderfactory.py

--- wscodec/decoder/decoderfactory.py
+++ wscodec/decoder/decoderfactory.py
@@ -2,19 +2,6 @@ from datetime import datetime
 from .exceptions import InvalidMajorVersionError, InvalidCircFormatError
 from .hdc2021 import HDC2021DecoderHT, HDC2021DecoderT
 
-def get_decoder(formatcode: int, statb64: str, timeintb64: str, circb64: str, usehmac: bool, secretkey: str,
-                scandatetime: datetime):
-    decoders = {
-        1: HDC2021DecoderHT,
-        2: HDC2021DecoderT
-    }
-    try:
-        decoder = decoders[formatcode](statb64, timeintb64, circb64, usehmac, secretkey, scandatetime)
-    except KeyError:
-        raise InvalidCircFormatError(formatcode)
-
-    return decoder
-
 
 class DecoderFactory:
     """
@@ -52,8 +39,30 @@ class DecoderFactory:
         if majorversion != 1:
             raise InvalidMajorVersionError
 
-        decoder = get_decoder(formatcode, statb64, timeintb64, circb64, usehmac, secretkey, scandatetime)
+        decoder = DecoderFactory.get_decoder(formatcode)(statb64, timeintb64, circb64, usehmac, secretkey, scandatetime)
         decoder.decode()
+        return decoder
+
+    @staticmethod
+    def get_decoder(formatcode: int):
+        """
+            Construct a Decoder object
+
+            Parameters
+            -----------
+            formatcode:
+                Value of the codec format field. Specifies which decoder shall be used.
+
+        """
+        decoders = {
+            1: HDC2021DecoderHT,
+            2: HDC2021DecoderT
+        }
+        try:
+            decoder = decoders[formatcode]
+        except KeyError:
+            raise InvalidCircFormatError(formatcode)
+
         return decoder
 
 
