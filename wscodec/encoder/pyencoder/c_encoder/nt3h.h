0b78076606125fcde9632d384c89344ebaee3f72
Malcolm Mackay <malcolm@plotsensor.com>
2021-04-09 14:17:50 +0100

    Brownout is not an error
    It occurs after every power up as a battery is inserted.
    
    https://e2e.ti.com/support/microcontrollers/msp430/f/msp-low-power-microcontroller-forum/296435/msp430f6438-sysrstiv-register
    
    https://e2e.ti.com/support/microcontrollers/msp430/f/msp-low-power-microcontroller-forum/132372/msp4305438-reset-problem

--- wscodec/encoder/pyencoder/c_encoder/nt3h.h
+++ wscodec/encoder/pyencoder/c_encoder/nt3h.h
@@ -31,7 +31,6 @@
 #ifndef _NT3H_H_
 #define _NT3H_H_
 
-void nt3h_init(void);
 int nt3h_writetag(int eepromBlock, char * blkdata);
 int nt3h_readtag(int eepromBlock, char * blkdata);
 int nt3h_eepromwritedone(void);
